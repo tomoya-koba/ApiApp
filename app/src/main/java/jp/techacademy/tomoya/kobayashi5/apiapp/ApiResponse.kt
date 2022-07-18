b37ecd4b6df7c31a4c1869f0750c168bbe8c2535
tomoya-koba <kobatmya@hotmail.co.jp>
2022-07-18 09:53:58 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/tomoya/kobayashi5/apiapp/ApiResponse.kt
@@ -0,0 +1,39 @@
+package jp.techacademy.tomoya.kobayashi5.apiapp
+import com.google.gson.annotations.SerializedName
+
+
+data class ApiResponse(
+    @SerializedName("results")
+    val results: Results
+)
+
+data class Results(
+    @SerializedName("api_version")
+    val apiVersion: String,
+    @SerializedName("results_available")
+    val resultsAvailable: Int,
+    @SerializedName("results_returned")
+    val resultsReturned: String,
+    @SerializedName("results_start")
+    val resultsStart: Int,
+    @SerializedName("shop")
+    val shop: List<Shop>
+)
+
+data class Shop(
+    @SerializedName("coupon_urls")
+    val couponUrls: CouponUrls,
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("logo_image")
+    val logoImage: String,
+    @SerializedName("name")
+    val name: String
+)
+
+data class CouponUrls(
+    @SerializedName("pc")
+    val pc: String,
+    @SerializedName("sp")
+    val sp: String
+)
---
+++ app/src/main/java/jp/techacademy/tomoya/kobayashi5/apiapp/FavoriteFragment.kt
@@ -0,0 +1,6 @@
+package jp.techacademy.tomoya.kobayashi5.apiapp
+
+import androidx.fragment.app.Fragment
+
+class FavoriteFragment: Fragment() {
+}
